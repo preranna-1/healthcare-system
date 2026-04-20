4dd58a340af19c68ac30c03ed2f255e5a2aa115e
preranna-1 <preranna.520@gmail.com>
2026-04-20 13:38:14 +0600

    Update healthcare_centers.php

--- backend/healthcare_centers.php
+++ backend/healthcare_centers.php
@@ -1,31 +1,14 @@
 <?php
-require_once '../db_connect.php';
+include 'db.php';
+
+$sql = "SELECT * FROM HEALTHCARE_CENTER";
+$result = $conn->query($sql);
+
+$data = [];
+while($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-if ($method === 'GET') {
-    $sql = "SELECT * FROM HEALTHCARE_CENTER";
-    $result = $conn->query($sql);
-    $centers = [];
-    while ($row = $result->fetch_assoc()) {
-        $centers[] = $row;
-    }
-    echo json_encode($centers);
-} 
-elseif ($method === 'POST') {
-    $data = json_decode(file_get_contents('php://input'), true);
-    
-    $sql = "INSERT INTO HEALTHCARE_CENTER (CenterID, Name, Address, PhoneNumbers, Email, Latitude, Longitude, OperatingHours, OverallRating, CenterType) 
-            VALUES ('{$data['CenterID']}', '{$data['Name']}', '{$data['Address']}', '{$data['PhoneNumbers']}', '{$data['Email']}', {$data['Latitude']}, {$data['Longitude']}, '{$data['OperatingHours']}', {$data['OverallRating']}, '{$data['CenterType']}')";
-    
-    if ($conn->query($sql)) {
-        echo json_encode(['success' => true, 'message' => 'Healthcare center created']);
-    } else {
-        echo json_encode(['success' => false, 'error' => $conn->error]);
-    }
-}
+echo json_encode($data);
 ?>
