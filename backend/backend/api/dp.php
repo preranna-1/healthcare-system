eaafd9d875e8c10db349a9b1c5717ca2b5ee1695
preranna-1 <preranna.520@gmail.com>
2026-04-20 13:37:40 +0600

    Update dp.php

--- backend/backend/api/dp.php
+++ backend/backend/api/dp.php
@@ -1,1 +1,13 @@
+<?php
+$host = "localhost";   // XAMPP default
+$user = "root";        // default user
+$pass = "";            // default password (empty unless you set one)
+$db   = "hospital_db"; // your database name
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 
